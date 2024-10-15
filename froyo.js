d880d7310554b9f6985421c99975913ea50b56db
kaijuxkitty <kaijuxkitty@gmail.com>
2024-10-15 17:55:26 -0500

    main

---
+++ froyo.js
@@ -0,0 +1,10 @@
+// create prompt for user flavor input
+//put user input into arrays
+//create object with key flavors
+//update object with new flavor inputs
+//create function
+
+let inputFlavors= prompt ('List of flavors:');
+  froyo = [`chocolate`,`vanilla`,`strawberry`];
+for (let i = 0; i < froyo.length; i++) 
+    console.log(froyo[i]); 
