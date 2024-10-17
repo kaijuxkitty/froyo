9e18764eb11a7b8c3f6f21b4a1137fc3f99182e9
kaijuxkitty <kaijuxkitty@gmail.com>
2024-10-17 17:58:21 -0500

    finished

--- froyo.js
+++ froyo.js
@@ -4,7 +4,27 @@
 //update object with new flavor inputs
 //create function
 
-let inputFlavors= prompt ('List of flavors:');
-  froyo = [`chocolate`,`vanilla`,`strawberry`];
-for (let i = 0; i < froyo.length; i++) 
-    console.log(froyo[i]); 
+let inputFlavors= prompt('List of flavors:');
+let froyo= [];
+for (let i = 0; i < inputFlavors.length; i++) {
+  froyo= inputFlavors.split(",");
+}
+console.log(froyo);
+
+let froyoObject = {};
+const froyoToObject =(froyoArray)=> {
+
+  for (let i= 0; i<froyoArray.length; i++) {
+    if (!froyoObject[froyoArray[i]]) { 
+      froyoObject[froyoArray[i]]=1;
+    } else { 
+     froyoObject[froyoArray[i]]+=1;
+    } 
+  }
+  return froyoObject
+};
+
+console.log(froyoToObject(froyo));
+
+
+    
